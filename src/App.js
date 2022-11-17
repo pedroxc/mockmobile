e97c91c69582ad3aab5298d92a543c75a0f39b12
Pedro Coelho <pedroxaviercoelho@hmail.com>
2022-11-17 18:03:34 -0300

    build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import layout from "./assets/layout.png";
+import topLayout from "./assets/layoutTop.png";
+import botLayout from "./assets/layoutBot.png";
+import { BotLayout, Layer, TopLayout } from "./styles";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopLayout src={topLayout} />
+      <Layer src={layout} />
+      <iframe
+        src="https://nwl-mobile.netlify.app/"
+        title="nwl"
+        style={{ height: 600 }}
+      />
+      <BotLayout src={botLayout} />
     </div>
   );
 }
-
-export default App;
---
+++ src/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Layer = styled.img`
+  z-index: -1;
+  position: fixed;
+`;
+export const TopLayout = styled.img`
+  z-index: 4;
+  position: fixed;
+`;
+export const BotLayout = styled.img`
+  z-index: 4;
+  position: fixed;
+  top: 600px;
+`;
